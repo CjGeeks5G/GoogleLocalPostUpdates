c334642b8a1f851185b384f5b5dabe75ca1cff24
CAMILO GARCIA CHACON <camilogarciach12@hotmail.com>
2024-10-18 06:16:45 -0500

    Add swagger

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -1,7 +1,8 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateChatCompletionRequest } from './dto/create-chat-completion-request.dto';
 import { ChatService } from './chat.service';
-
+import { ApiTags } from '@nestjs/swagger';
+@ApiTags("Chat Request")
 @Controller('chat')
 export class ChatController {
   constructor(private readonly chatService: ChatService) {}
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true}));
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  const config = new DocumentBuilder()
+    .setTitle('Chatbot App')
+    .setDescription('This is Wizybot’s  technical assessment')
+    .setVersion('1.0')
+    .addTag('chatbot')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
